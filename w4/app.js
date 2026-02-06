216cffc882b034d71f84113f6b2dc6bdd99ed64f
Ryan Mooneyham <Rkmooneyham2@my.scccd.edu>
2026-02-05 20:39:50 -0800

    Step 2 Complete

---
+++ w4/app.js
@@ -0,0 +1,33 @@
+console.log('Hello from app.js! Your JavaScript is connected and runing!')
+
+import * as orderHandler from './order-handler.js';
+import * as priceCalculator from './price-calculator.js';
+
+const orderForm = document.getElementById('order-form');
+const orderSummary = document.getElementById('order-summary');
+
+
+const handleOrderSubmit = function (event) {
+    event.preventDefault();
+    const inputs = orderHandler.getOrderInputs();
+    console.log(inputs);
+    const messege = `Ordered ${inputs.qty} ${inputs.size} where gift wrap is ${inputs.giftWrap}`;
+    // When the submit button is pushed the above lines prevent the default submit behavior and create and object called "inputs".
+    // We pass that "inputs" object to the calculateTotal function and call the function with the line below.
+    const total = priceCalculator.calculateTotal(inputs)
+    console.log("is it returning the total price now? >>>", total.totalPrice);
+    console.log(messege);
+    orderSummary.textContent = messege;
+
+
+};
+
+document.addEventListener('DOMContentLoaded', function(){
+    console.log(`DOM fully loaded and parsed, App is ready for interaction`);
+    init();
+});
+
+const init = function() {
+    orderForm.addEventListener('submit', handleOrderSubmit);
+    console.log('app initialized')
+}
